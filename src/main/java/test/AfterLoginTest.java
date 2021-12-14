6bb1f40af394fdd3209e95046a72ce7d8705bec9
kashvini <kunchalashvini@gmail.com>
2021-12-14 13:28:03 +0000

    test

---
+++ src/main/java/test/AfterLoginTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import pages.AfterLoginPage;
+import pages.LoginPage;
+
+
+public class AfterLoginTest extends Baseclass {
+
+	
+	@Parameters({"Email","Password"})
+	@Test
+	public void afterLogin(String Emailval,String Passwordval) {
+		AfterLoginPage AfterLogin = new AfterLoginPage();
+		AfterLogin.afterlogin(Emailval, Passwordval);
+		
+		
+	}
+
+}
